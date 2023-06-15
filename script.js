162eadb225e7263c7dc99ef6c289df953bfeae81
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-16 00:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // your code here
+
+ document.getElementById("form").addEventListener("submit", function(event){
+        event.preventDefault()
+    });
+
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+    let button = document.getElementById("button");
+
+    button.addEventListener("click", function(){
+        button.classList.remove("button");
+        button.classList.add("button-blue");
+    })
+
+    button.addEventListener("blur", function(){
+        button.classList.remove("button-blue");
+        button.classList.add("button");
+    })
+
+	let url = document.getElementById("url");
+
+    console.log(`name ----->` + name);
+    console.log(year);
+	
+	if(name !== '' && year === ''){
+        url.innerHTML = `https://localhost:8080/`+`?name=${name}`; 
+    }else if(name === '' && year !== ''){
+        url.innerHTML = `https://localhost:8080/`+`?year=${year}`;
+    }else if(name !== '' && year !== ''){
+        url.innerHTML = `https://localhost:8080/` +`?name=${name}&year=${year}`;
+    }
